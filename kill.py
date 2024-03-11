77951aa7fc26895b60eb25537527ec722b23a094
Akshay Narayan <akshayn@icsi.berkeley.edu>
2024-03-11 13:20:22 -0700

    remove old scripts

--- kill.py
+++
@@ -1,9 +0,0 @@
-#import os
-import subprocess
-f = open("a.txt","r")
-lines = f.readlines()
-f.close()
-lines = lines[0].strip()
-for line in lines.split():
-    #print line
-    subprocess.Popen("sudo kill -9 "+line,shell=True)
